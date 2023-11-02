aaad1d4e9af01f7ce83c49a15efb5c60bca729f7
George Higbie <gehigbie@gmail.com>
2023-11-02 18:15:26 -0500

    Add get wallet balance

--- index.js
+++ index.js
@@ -8,5 +8,24 @@ const {
 
 const wallet = new Keypair();
 
-const publicKey = wallet._keypair.publicKey;
+const publicKey = new PublicKey(wallet._keypair.publicKey);
 const secretKey = wallet._keypair.secretKey;
+
+// console.log(publicKey);
+// console.log(secretKey);
+
+const getWalletBalance = async () => {
+  try {
+    const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+    const walletBalance = await connection.getBalance(publicKey);
+    console.log(`Wallet ballance: ${walletBalance}`);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const main = async () => {
+  await getWalletBalance();
+};
+
+main();
