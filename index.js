a37dc4a411f59bc0e8036fd6601dfd8ccd9e3cc9
George Higbie <gehigbie@gmail.com>
2023-11-02 18:08:17 -0500

    Fix import errors

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const { Connection, PublicKey, clusterApiUrl, KeyPair, LAMPORTS_PER_SOL } =
-  requiere("@solana/web3.js");
+const {
+  Connection,
+  PublicKey,
+  clusterApiUrl,
+  Keypair,
+  LAMPORTS_PER_SOL,
+} = require("@solana/web3.js");
 
-const wallet = new KeyPair();
+const wallet = new Keypair();
 
 const publicKey = wallet._keypair.publicKey;
 const secretKey = wallet._keypair.secretKey;
